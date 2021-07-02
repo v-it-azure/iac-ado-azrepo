7a04ebdac05a513bf6681abbb369cae64b2b0fee
Eric Pingel <epingel@vermeer.com>
2021-07-02 12:12:07 -0500

    add in default value for ado url

--- setup/variables.tf
+++ setup/variables.tf
@@ -1,6 +1,7 @@
 variable "ado_org_service_url" {
   type        = string
   description = "Org service url for Azure DevOps"
+  default  = "https://dev.azure.com/v-iac-az/"
 }
 
 variable "ado_github_repo" {
